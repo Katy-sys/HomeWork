dd57e149243ffa859fe24625076c30a4f6f462b6
Catherine <catherine.zelenia@gmail.com>
2023-05-29 17:16:02 +0300

    Add HM01_Task06 add Definition of even or odd number

---
+++ HM01_Task06_EvenOrOdd/Program.cs
@@ -0,0 +1,31 @@
+﻿//Definition of even or odd number
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+bool DefEvenOrOdd (int x)
+{
+    if (x % 2 == 0)
+    {
+        return true; 
+    }
+    
+    else
+    {
+        return false;
+    }
+}
+
+//Console.Clear();
+
+Console.WriteLine("Enter the number");
+int x =Convert.ToInt32(Console.ReadLine());
+
+Console.Write("The number is even. - It's ");
+string defnum = Convert.ToString(DefEvenOrOdd(x));
+Console.Write(defnum);
+
+
