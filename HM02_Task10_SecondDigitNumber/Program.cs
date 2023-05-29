f3daffc2f27db2f0349d1f6776f53b3cc1d3a9c2
Catherine <catherine.zelenia@gmail.com>
2023-05-30 00:10:34 +0300

    add HM02_Task10_SecondDigitNumber add the array is filled with digits of the number and the choice of the second value of the number is added

---
+++ HM02_Task10_SecondDigitNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿// Second digit of the number
+
+// Задача 10: Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру
+// этого числа. Через строку решать нельзя.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine("Enter a three-digit number");
+int n = Convert.ToInt32(Console.ReadLine());
+int numberDigits = (int)Math.Log10(n) + 1;
+
+
+int i=numberDigits-1;
+int [] array = new int [numberDigits];
+while (i >= 0)
+{
+    array[i] = n % 10;
+    i--;
+    n = (int)n/10;
+}
+
+
+Console.WriteLine(array[1]);
+
+
