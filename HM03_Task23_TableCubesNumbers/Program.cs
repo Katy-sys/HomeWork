08a74be3824d8714c5a1119463b6a63f8f4ea547
Catherine <catherine.zelenia@gmail.com>
2023-05-31 22:56:31 +0300

    add HM03_Task23_TableCubesNumbers Created a table of cubes of numbers from 1 to N.

---
+++ HM03_Task23_TableCubesNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int number = AcceptMessageGiveResult("Введите целое число");
+
+int AcceptMessageGiveResult (string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+Console.WriteLine("|\tX\t|\tX3\t|");
+Console.WriteLine("---------------------------------");
+
+for (int j=1; j<=number; j++)
+    Console.WriteLine($"|\t{j}\t|\t{Math.Pow(j, 3)}\t|");
+
+Console.WriteLine("---------------------------------");
+
