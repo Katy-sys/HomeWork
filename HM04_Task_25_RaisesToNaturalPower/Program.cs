8c22581b7f0e95472451f9157bcc212281c91992
Catherine <catherine.zelenia@gmail.com>
2023-06-01 00:11:07 +0300

    add HM04_Task_25_RaisesToNaturalPower and added exponentiation function with positive value.

---
+++ HM04_Task_25_RaisesToNaturalPower/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит
+// число A в натуральную степень B. Math.Pow юзать нельзя.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int AcceptMessageGiveResult (string message)
+{
+    System.Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int RaisesToNaturalPower (int number, int degree)
+{
+    int numberInSt = 1;
+    for (int i=1; i<=degree; i++)
+        numberInSt = numberInSt*number;
+    return numberInSt;
+}
+
+int numberA = AcceptMessageGiveResult("Введите целое число A");
+int numberB = AcceptMessageGiveResult("Введите целое число B");
+
+System.Console.WriteLine(RaisesToNaturalPower(numberA, numberB));
+    
+
