fd543cf6d433be06919115e036fd534ae8aafd3f
Catherine <catherine.zelenia@gmail.com>
2023-06-09 01:16:33 +0300

    add HM06_Task41_PositiveNumbers and Received numbers from the console entered through a space and counted the number of positive ones among them

---
+++ HM06_Task41_PositiveNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+// Метод получения значения с консоли (для длинны массива)
+int[] GetNumber (string message)
+{
+    System.Console.WriteLine(message);
+    string[] line = Console.ReadLine().Split().ToArray()!;
+    int[] lineInt = new int[line.Length];
+    for (int i = 0; i < line.Length; i++)
+    {
+        lineInt[i] = Convert.ToInt32(line[i]);
+    }
+
+    return lineInt;
+}
+
+// Подсчет колличества чисел, которые больше нуля
+int NumRes (int[] array)
+{
+    int result = 0;
+    foreach (var item in array)
+    {
+        if (item > 0)
+            result = result + 1;
+    }
+    return result;
+}
+
+int[] lenghtArray = GetNumber("Введите несколько чисел через пробел: ");
+System.Console.WriteLine(NumRes(lenghtArray));
+
+
+
