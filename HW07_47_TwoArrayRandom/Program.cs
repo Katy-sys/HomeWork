3f61a263b60a3880e163bfcccfb76adc172d4367
zelv <zelv@>
2023-06-13 08:32:41 +0300

    add HW07_47_TwoArrayRandom _ HW07_50_NumberInTwoArray _and_ HW07_52_AverageTwoArray

---
+++ HW07_47_TwoArrayRandom/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+void FillArray(double[,] array)
+{   
+    Random number = new Random();
+    for (int i =0;i<array.GetLength(0);i++)
+    for (int j =0;j<array.GetLength(1);j++)
+        array[i,j] = number.NextDouble() * (10.00 + 9.99) - 9.99;
+}
+
+int Ost (double n)
+{
+    int whole = (int) n;
+    double frac = n - whole;
+    
+    int m = (int)(frac*10);
+    return m;
+}
+
+double FormatN (double n)
+{
+    double m = Math.Round(n, 1);
+    return m;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i =0;i<array.GetLength(0);i++)
+        {
+        for (int j =0;j<array.GetLength(1);j++)
+            Console.Write($"{array[i,j],4}    ");
+        System.Console.WriteLine();
+        }
+}
+
+void PrintArrayFormat(double[,] array)
+{
+    double n;
+    for (int i =0;i<array.GetLength(0);i++)
+    {
+        for (int j =0;j<array.GetLength(1);j++)
+        { 
+            n = FormatN(array[i,j]);
+            if(n == 0)
+                Console.Write($"{string.Format("{0:N0}", (int)n, 4)}   ");
+            else if(Ost(n) == 0)
+                Console.Write($"{string.Format("{0:N0}", n, 4)}   ");
+            else
+                Console.Write($"{string.Format("{0:N1}", n, 4)}   ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк m: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов n:");
+int cols = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double [rows,cols];
+FillArray(array);
+PrintArray(array);
+PrintArrayFormat(array);
---
+++ HW07_50_NumberInTwoArray/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает
+// значение элемента в двумерном массиве, и возвращает позицию
+// этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+
+void FillArray(int[,] array)
+{
+    Random number = new Random();
+    for (int i =0;i<array.GetLength(0);i++)
+        for (int j =0;j<array.GetLength(1);j++)
+            array[i,j] = number.Next(10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i =0;i<array.GetLength(0);i++)
+        {
+        for (int j =0;j<array.GetLength(1);j++)
+            Console.Write($"{array[i,j],1}    ");
+        System.Console.WriteLine();
+        }
+}
+
+int NumQuantity (int[,] array, int number)
+{
+    int n = 0;
+    for (int i =0; i<array.GetLength(0); i++)
+        for (int j =0; j<array.GetLength(1); j++)
+            if (array[i,j] == number)
+                n++;
+    return n;        
+}
+
+void NumSearchPosition (int[,] array, int number, int quantity)
+{
+    int[,] arrayRes = new int [quantity,2];
+    int n = 0;
+    for (int i =0; i<array.GetLength(0); i++)
+        for (int j =0; j<array.GetLength(1); j++)
+            if (array[i,j] == number)
+            {   
+                arrayRes[n,0] = i;
+                arrayRes[n,1] = j;
+                n++;
+            }
+    PrintArray(arrayRes);  
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк m: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов n:");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [rows,cols];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine("Введите число для поиска ");
+int numSearch = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("");
+int numQ = NumQuantity(array, numSearch);
+if (numQ == 0)
+    Console.WriteLine($"Число {numSearch} не найдено");
+
+else
+    NumSearchPosition(array, numSearch, numQ);
