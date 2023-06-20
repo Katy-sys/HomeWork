08c27195266fc8ec7e76306c0fd57e720178795b
Catherine <catherine.zelenia@gmail.com>
2023-06-20 09:46:26 +0300

    add added check for non-repetition of random numbers HM08_Task60_3DArray

--- HM08_Task60_3DArray/Program.cs
+++ HM08_Task60_3DArray/Program.cs
@@ -5,17 +5,44 @@
 // 66(0,0,0) 25(0,1,0)
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
-// 26(1,0,1) 55(1,1,1)
+// 26(1,0,1) 55(1,1,1)  Math.Pow(x, stepen)
 
-void FillArray(int[, , ] arr)
+bool IsElementExistInArray (int[, , ] arr, int genNum)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
             for (int k = 0; k < arr.GetLength(2); k++)
-            {
-                arr[i, j, k] = new Random().Next(100);
+            {     
+                if (arr[i, j, k] == genNum)
+                {
+                    return true;
+                }
+            }
+        }    
+    }
+    return false;
+      
+}
+
+void FillArray(int[, , ] arr, int size)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {             
+                int genNum = new Random().Next(100);
+
+                while (IsElementExistInArray(arr, genNum) == true)
+                {
+                    genNum = new Random().Next(100);
+                }
+
+                arr[i, j, k] = genNum;
+                
             }
         }
     }
@@ -36,9 +63,11 @@ void PrintArray(int[, , ] arr)
     }
 }
 
+
+int size = 2;
 Console.Clear();
-int[, , ] array = new int[2, 2, 2];
-FillArray(array);
+int[, , ] array = new int[size, size, size];
+FillArray(array, size);
 PrintArray(array);
 
 
