ae156f811e3721d351c5c6f9d3a74d76f217766c
Catherine <catherine.zelenia@gmail.com>
2023-06-21 11:40:57 +0300

    added tasks using recursion HM09_Task64_OutputOfNumRec, HM09_Task66_SumRec, HM09_Task66_SumRec

---
+++ HM09_Task64_OutputOfNumRec/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int GetNumber (string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(System.Console.ReadLine());
+    return result;
+}
+
+int OutputOfNumGap (int num)
+{
+    if (num == 1) return 0;
+    System.Console.Write($"{num}, ");
+    if (num == 2) System.Console.Write($"{num-1}");
+    return OutputOfNumGap(num-1);
+}
+
+//Console.Clear();
+int num = GetNumber("Задайте число N: ");
+System.Console.WriteLine();
+System.Console.Write("\"");
+OutputOfNumGap(num);
+System.Console.Write("\"");
+
+
+ 
+
---
+++ HM09_Task66_SumRec/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetNumber (string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(System.Console.ReadLine());
+    return result;
+}
+
+int SumNumInInterval(int numStart, int numFin)
+{
+    if (numStart > numFin) return 0;
+    return numStart + SumNumInInterval(numStart+1, numFin);
+}
+
+//Console.Clear();
+int numM = GetNumber("Задайте число M: ");
+int numN = GetNumber("Задайте число N: ");
+
+if (numM > numN)
+    (numM, numN) = (numN, numM);
+
+System.Console.WriteLine(SumNumInInterval(numM, numN));
+
